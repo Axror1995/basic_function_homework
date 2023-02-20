7d8d39b584b86875d587038af7d48e923a3ddae9
Axror1995 <axrorraximov108@gmail.com>
2023-02-20 10:52:21 +0900

    05

--- bin/basic_func05.dart
+++ bin/basic_func05.dart
@@ -1,4 +1,10 @@
 // Create a function called func.
 
 // Return the value double type.
-void main() {}
+
+String func(){
+  return '-1.0, '+' 0.1, '+' 0.2, '+' 1.0,';
+}
+void main() {
+  print(func());
+}
